bd983d55c27f67b5ea5680a37534cb02f3e5aec4
sudiptapanda2004 <sanjupanda2003@gmail.com>
2026-03-28 21:07:21 +0530

    a newer implementation and addition of code from Array12 to Array13

--- src/src/Array12.java
+++ src/src/Array12.java
@@ -1,6 +1,7 @@
 // Printing sub-arrays - continuous part of a array
 
 public static void printSubarrays(int numbers[]) {
+    int ts = 0;                            // ts = n*(n+1)/2
     for (int i=0; i< numbers.length; i++) {
         // int start = numbers[i];
         for (int j=i; j<numbers.length; j++) {
@@ -11,11 +12,12 @@ public static void printSubarrays(int numbers[]) {
                 System.out.print(numbers[k]);
                 if (k < j) System.out.print(" ");   // space in between for output format
             }
-
+            ts++;
             System.out.println("]");        // array ending bracket for output format
         }
         System.out.println();
     }
+    System.out.println("The total no. of sub-arrays printed are : "+ts);
 }
 
 void main() {
---
+++ src/src/Array13.java
@@ -0,0 +1,48 @@
+// Printing sub-arrays - continuous part of a array with sum, maxSum and minSum
+
+public static void printSubarrays(int numbers[]) {
+    int ts = 0;                             // ts = n*(n+1)/2
+    int largestSum = Integer.MAX_VALUE;     // initializing with +infinity
+    int smallestSum = Integer.MIN_VALUE;       // initializing with -infinity
+
+    for (int i=0; i< numbers.length; i++) {
+        // int start = numbers[i];
+        for (int j=i; j<numbers.length; j++) {
+            // int end = numbers[j];
+            System.out.print("[");          // array starting bracket for output format
+
+            int sum = 0;                    // reset sum for each sub-array
+
+
+            for (int k=i; k<=j; k++) {      // this loop's task is only to print
+
+                sum = sum + numbers[k];     // calculation and accumulation of each new sum
+
+                System.out.print(numbers[k]);
+                if (k < j) System.out.print(" ");   // space in between for output format
+            }
+            if (largestSum > sum) {
+                largestSum = sum;
+            }
+            if (smallestSum < sum) {
+                smallestSum = sum;
+            }
+
+            ts++;
+            System.out.println("]");        // array ending bracket for output format
+            System.out.println("Sum of this sub-array : "+sum);     // sum of each sub-array is printed
+        }
+        System.out.println();
+    }
+    System.out.println("The total no. of sub-arrays printed are : "+ts);
+    System.out.println("The largest sum of the above sub-arrays is : "+largestSum);
+    System.out.println("The smallest sum of the above sub-arrays is : "+smallestSum);
+
+}
+
+void main() {
+    int numbers[] = {1,2,3,4,5};
+
+    System.out.println("A");
+    printSubarrays(numbers);
+}
